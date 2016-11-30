f45224cb420bbf69b3309f0f95d10467e953779c
Nícolas Maduro <nicolasamaduro@gmail.com>
2016-11-30 11:04:43 -0200

    bug loop

--- automatic_heatmap/bgsubtractor.cpp
+++ automatic_heatmap/bgsubtractor.cpp
@@ -1,5 +1,6 @@
 #include "bgsubtractor.h"
 #include <time.h>
+#include <iostream>
 
 Mat openStructuringElement = getStructuringElement(MORPH_RECT, Size(2,2));
 Mat closeStructuringElement = getStructuringElement(MORPH_RECT, Size(5,7));
@@ -13,29 +14,28 @@ Mat processVideoSilently(VideoCapture *capture, Ptr<BackgroundSubtractor> bgSubt
 	unsigned maxValue = 0;
 	int i = 0;
 	
-	capture->read(frame);
+        capture->read(frame);
 	accumulator = Mat::zeros(frame.size(), CV_32SC1);
 	
-	do{
-		bgSubtractor->apply(frame, fgMask);
+        do{
+                bgSubtractor->apply(frame, fgMask);
 		threshold(fgMask, binary, 128, 255, THRESH_BINARY);
 		morphologyEx(binary, binary, MORPH_OPEN, openStructuringElement);
 		morphologyEx(binary, binary, MORPH_CLOSE, closeStructuringElement);
-		
-		for(int row=0; row < accumulator.rows; ++row){
+                for(int row=0; row < accumulator.rows; ++row){
 			uchar *f = binary.ptr(row);
 			unsigned *a = (unsigned*)accumulator.ptr(row);
 			for(int col = 0; col < accumulator.cols; ++col){
 				a[col] += f[col];
 				maxValue = max(maxValue, a[col]);
-			}
+                        }
+
 		}
-		printf("\r%d", ++i);
-		fflush(stdout);
-		printf("\r      ");
-	}while(capture->read(frame));
-	
-	accumulator.convertTo(heatmap, CV_32FC1, 1.0/maxValue);
+                printf("\r%d", ++i);
+                fflush(stdout);
+                printf("\r      ");
+	} while(capture->grab() && capture->retrieve(frame));
+        accumulator.convertTo(heatmap, CV_32FC1, 1.0/maxValue);
 	return heatmap;
 
 }
@@ -79,7 +79,7 @@ Mat processVideo(VideoCapture *capture, Ptr<BackgroundSubtractor> bgSubtractor, 
 		end = clock();
 		elapsed = double(end - start)/ CLOCKS_PER_SEC;
 		keyboard = waitKey(max(1.0,30.0-elapsed));
-	}while( (char)keyboard != 'q' && (char)keyboard != 27 && (char)keyboard != 'p' && capture->read(frame));
+	}while( (char)keyboard != 'q' && (char)keyboard != 27 && (char)keyboard != 'p' && capture->grab() && capture->retrieve(frame));
 	
 	if( (char)keyboard == 'q' || (char)keyboard == 27){
 		exit(0);
